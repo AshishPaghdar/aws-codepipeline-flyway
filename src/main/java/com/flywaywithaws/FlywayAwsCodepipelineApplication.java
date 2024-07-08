2799a69a7743ed1c66547cbd2d0434808c7438b2
Deep-Inexture <deeppanchal@inexture.com>
2024-07-08 13:25:05 +0530

    Initial Commit

---
+++ src/main/java/com/flywaywithaws/FlywayAwsCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.flywaywithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayAwsCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayAwsCodepipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flywaywithaws/FlywayAwsCodepipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flywaywithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayAwsCodepipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
